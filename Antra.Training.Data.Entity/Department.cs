dec24becf968001870ba059fd47dabef90aa6cb8
sharm <sharm@LAPTOP-TALM7IV0>
2022-02-22 14:26:31 -0600

    Add project files.

---
+++ Antra.Training.Data.Entity/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antra.Training.Data.Entity
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+        public string Name { get; set; }
+        public decimal Budget { get; set; }
+        public DateTime StartDate { get; set; }
+        public int? InstructorID { get; set; }
+        public object RowVersion { get; set; }
+
+        //navigatonal properties
+        public List<Person> Persons { get; set; } = new List<Person>();
+    }
+}
